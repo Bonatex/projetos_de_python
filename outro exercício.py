77ccd34a02e04aca214671aba1b1cc021c7c3ca1
João_Pedro <bonatexjp@gmail.com>
2025-09-20 16:03:55 -0300

    mensagem

---
+++ outro exercício.py
@@ -0,0 +1,24 @@
+# Programa de dizer olá mundo!
+
+print(f"Olá, mundo!")
+
+#Programa de soma
+numero1 = 12
+numero2 = 14
+soma = numero1 + numero2
+
+print ("A soma dos dois números,12 e 14, é: ", soma)
+
+#Calculadora de volume com tamanhos pré - definidos
+compr = 12
+alt = 20
+larg = 14
+volume = compr * alt * larg
+
+print ("Volume da forma geométrica sugerida é: ", volume,"cm\u00b3")
+
+preco_unitario = 12.40
+quant = 3
+preco_total = preco_unitario * quant
+print (f"O preço unitário da cadeira infantil é :R$ {preco_unitario:.2f}, sendo a quantidade comprada {quant}, dando um total de R$ {preco_total:.2f} ")
+
---
+++ python exercicios.py
@@ -0,0 +1,21 @@
+nome = "Josué"
+sobrenome = "Pedro"
+idade = 28
+idade1 = str(idade)
+idade2 = str(28)
+pais = "Brasil"
+x = int (input ("Digite um número:"))
+y = int (input ("Digite um número:"))
+print ("Meu nome é " + nome +" "+ sobrenome )
+print ("Moro em " + pais + "\ntenho " + idade1 + " anos")
+print ("Moro em " + pais + " e tenho " + str(idade) + " anos")
+print ("Moro em " + pais + " e tenho " + idade2 + " anos")
+print (f"Moro em {pais} \ne tenho {idade} anos")
+#diferentes maneiras de mostrar uma string
+'''
+print ("banana")
+'''
+
+resultado = int(x) * int(y)
+print (resultado)
+print (type(x))
